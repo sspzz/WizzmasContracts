2a3c9dbd83763792adb62bf2c780b9bb84030fa6
sspzz <richiefilth@protonmail.com>
2022-11-22 20:42:38 +0100

    Add beasts and ponies

--- script/Wizzmas.s.sol
+++ script/Wizzmas.s.sol
@@ -2,38 +2,47 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "../src/WizzmasArtwork.sol";
 import "../src/WizzmasArtworkMinter.sol";
 import "../src/WizzmasCard.sol";
 
 contract WizzmasScript is Script {
     function run() external {
-        uint256 numArtworkTypes = 3;
-        string memory cardsBaseURI = vm.envString("BASE_URI_CARDS");
-        string memory artworksBaseURI = vm.envString("BASE_URI_ARTWORKS");
-        address wizardsAddress = vm.envAddress("CONTRACT_ADDRESS_WIZARDS");
-        address soulsAddress = vm.envAddress("CONTRACT_ADDRESS_SOULS");
-        address warriorsAddress = vm.envAddress("CONTRACT_ADDRESS_WARRIORS");
-        address poniesAddress = vm.envAddress("CONTRACT_ADDRESS_PONIES");
-
         vm.startBroadcast();
+
+        // Artworks
+        uint256 numArtworkTypes = 1;
         WizzmasArtwork artwork = new WizzmasArtwork();
-        artwork.setTokenURI(0, string.concat(artworksBaseURI, "0"));
-        artwork.setTokenURI(1, string.concat(artworksBaseURI, "1"));
-        artwork.setTokenURI(2, string.concat(artworksBaseURI, "2"));
+        for (uint i = 0; i < numArtworkTypes; i++) {
+            artwork.setTokenURI(
+                i,
+                string.concat(
+                    vm.envString("BASE_URI_ARTWORKS"),
+                    Strings.toString(i)
+                )
+            );
+        }
+
+        // Artworks Minter
         WizzmasArtworkMinter artworkMinter = new WizzmasArtworkMinter(
             address(artwork),
             numArtworkTypes
         );
         artwork.addMinter(address(artworkMinter));
+
+        // Cards
         WizzmasCard card = new WizzmasCard(
             address(artwork),
-            wizardsAddress,
-            soulsAddress,
-            warriorsAddress,
-            poniesAddress,
-            cardsBaseURI
+            vm.envAddress("CONTRACT_ADDRESS_WIZARDS"),
+            vm.envAddress("CONTRACT_ADDRESS_SOULS"),
+            vm.envAddress("CONTRACT_ADDRESS_WARRIORS"),
+            vm.envAddress("CONTRACT_ADDRESS_PONIES"),
+            vm.envAddress("CONTRACT_ADDRESS_BEASTS"),
+            vm.envAddress("CONTRACT_ADDRESS_SPAWN"),
+            vm.envString("BASE_URI_CARDS")
         );
+
         vm.stopBroadcast();
     }
 }
