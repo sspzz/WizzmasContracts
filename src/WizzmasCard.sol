a377e80e53eca4d2c59abaae1c78ece6eaea29b8
sspzz <richiefilth@protonmail.com>
2022-11-29 11:56:57 +0100

    Remove ERC721Enumerable

--- src/WizzmasCard.sol
+++ src/WizzmasCard.sol
@@ -6,20 +6,20 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract WizzmasCard is
     ERC721,
-    ERC721Enumerable,
     ERC721Burnable,
     Ownable,
     ReentrancyGuard
 {
     struct Card {
+        uint256 card;
         address tokenContract;
         uint256 token;
         uint256 artwork;
@@ -27,8 +27,11 @@ contract WizzmasCard is
         address sender;
         address recipient;
     }
-    event WizzmasCardMinted(Card data);
     mapping(uint256 => Card) cards;
+    event WizzmasCardMinted(Card data);
+    
+    using Counters for Counters.Counter;
+    Counters.Counter private nextTokenId;
 
     address public artworkAddress;
 
@@ -43,7 +46,6 @@ contract WizzmasCard is
     string public baseURI;
 
     bool public mintEnabled = false;
-
 
     string[] public messages = [
         "Have a very Merry Wizzmas!",
@@ -80,7 +82,6 @@ contract WizzmasCard is
         setBaseURI(_initialBaseURI);
     }
 
-    // You must own a wizard to perform the spell
     function mint(
         address _tokenContract,
         uint256 _tokenId,
@@ -109,18 +110,12 @@ contract WizzmasCard is
             "NO_ARTWORK"
         );
 
-        // Burn the Artwork?
-        // ERC1155Burnable(artworkAddress).burn(
-        //     _msgSender(),
-        //     _artworkId,
-        //     1
-        // );
-
-        // Mint the Card
-        uint256 newId = totalSupply();
+        uint256 newId = nextTokenId.current();
         _safeMint(_recipient, newId);
+        nextTokenId.increment();
         
         cards[newId] = Card(
+            newId,
             _tokenContract,
             _tokenId,
             _artworkId,
@@ -132,7 +127,7 @@ contract WizzmasCard is
     }
 
     function getCard(uint256 cardId) public view returns (Card memory) {
-        if (totalSupply() > cardId) {
+        if (nextTokenId.current() > cardId) {
             return cards[cardId];
         }
         revert("CARD_NOT_MINTED");  
@@ -148,42 +143,6 @@ contract WizzmasCard is
 
     function _baseURI() internal view virtual override returns (string memory) {
         return baseURI;
-    }
-
-    function tokensOfOwner(address _owner)
-        external
-        view
-        returns (uint256[] memory)
-    {
-        uint256 tokenCount = balanceOf(_owner);
-        if (tokenCount == 0) {
-            return new uint256[](0);
-        } else {
-            uint256[] memory result = new uint256[](tokenCount);
-            uint256 index;
-            for (index = 0; index < tokenCount; index++) {
-                result[index] = tokenOfOwnerByIndex(_owner, index);
-            }
-            return result;
-        }
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId,
-        uint256 batchSize
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 
     // Only contract owner shall pass
